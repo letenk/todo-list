9b62633708f5b0f5b2af34bb29e1ad8337766581
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-11-02 14:45:12 +0700

    features: todo repository FindAll, FindByActivityGroupID,, test

--- repository/todo_repository.go
+++ repository/todo_repository.go
@@ -8,6 +8,7 @@ import (
 type TodoRepository interface {
 	Save(todo domain.Todo) (domain.Todo, error)
 	FindAll() ([]domain.Todo, error)
+	FindByActivityGroupID(activityGroupID uint64) ([]domain.Todo, error)
 	FindOne(id uint64) (domain.Todo, error)
 	Update(todo domain.Todo) (domain.Todo, error)
 	Delete(todo domain.Todo) (bool, error)
@@ -21,6 +22,39 @@ func NewRepositoryTodo(db *gorm.DB) *todoRepository {
 	return &todoRepository{db}
 }
 
+func (r *todoRepository) FindAll() ([]domain.Todo, error) {
+	var todos []domain.Todo
+
+	err := r.db.Find(&todos).Error
+	if err != nil {
+		return todos, nil
+	}
+
+	return todos, nil
+}
+
+func (r *todoRepository) FindByActivityGroupID(activityGroupID uint64) ([]domain.Todo, error) {
+	var todos []domain.Todo
+
+	err := r.db.Where("activity_group_id = ?", activityGroupID).Find(&todos).Error
+	if err != nil {
+		return todos, nil
+	}
+
+	return todos, nil
+}
+
+func (r *todoRepository) FindOne(id uint64) (domain.Todo, error) {
+	var todo domain.Todo
+
+	err := r.db.Where("id = ?", id).Find(&todo).Error
+	if err != nil {
+		return todo, nil
+	}
+
+	return todo, nil
+}
+
 func (r *todoRepository) Save(todo domain.Todo) (domain.Todo, error) {
 	err := r.db.Create(&todo).Error
 	if err != nil {
@@ -28,4 +62,22 @@ func (r *todoRepository) Save(todo domain.Todo) (domain.Todo, error) {
 	}
 
 	return todo, nil
+}
+
+func (r *todoRepository) Update(todo domain.Todo) (domain.Todo, error) {
+	err := r.db.Save(&todo).Error
+	if err != nil {
+		return todo, err
+	}
+
+	return todo, nil
+}
+
+func (r *todoRepository) Delete(todo domain.Todo) (bool, error) {
+	err := r.db.Delete(&todo).Error
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
--- test/todo_repository_test.go
+++ test/todo_repository_test.go
@@ -1,13 +1,16 @@
 package test
 
 import (
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/letenk/todo-list/helper"
 	"github.com/letenk/todo-list/models/domain"
 	"github.com/letenk/todo-list/repository"
 	"github.com/rizkydarmawan-letenk/jabufaker"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func createRandomTodoRepository(t *testing.T) domain.Todo {
@@ -42,7 +45,184 @@ func createRandomTodoRepository(t *testing.T) domain.Todo {
 	return newTodo
 }
 
+func TestFindAllTodoRepository(t *testing.T) {
+	t.Parallel()
+	var mutex sync.Mutex
+	// Create some random data
+	for i := 0; i < 10; i++ {
+		go func() {
+			mutex.Lock()
+			createRandomTodoRepository(t)
+			mutex.Unlock()
+		}()
+	}
+
+	todoRepository := repository.NewRepositoryTodo(ConnTest)
+
+	// Find all
+	todos, err := todoRepository.FindAll()
+	helper.ErrLogPanic(err)
+
+	assert.NoError(t, err)
+	assert.NotEqual(t, 0, len(todos))
+
+	for _, data := range todos {
+		require.NotEmpty(t, data.ID)
+		require.NotEmpty(t, data.Title)
+		require.NotEmpty(t, data.ActivityGroupID)
+		require.NotEmpty(t, data.Priority)
+		require.NotEmpty(t, data.CreatedAt)
+		require.NotEmpty(t, data.UpdatedAt)
+
+		require.NotNil(t, data.IsActive)
+
+		require.Empty(t, data.DeletedAt)
+	}
+
+}
+
+func TestFindByActivityGroupTodoRepository(t *testing.T) {
+	t.Parallel()
+	var mutex sync.Mutex
+	// todos for store `new data todos` from channel
+	var todos []domain.Todo
+
+	// channel for store data `new data todos` from process create random todo
+	channel := make(chan domain.Todo)
+	defer close(channel)
+
+	// Create some random data
+	for i := 0; i < 10; i++ {
+		go func() {
+			mutex.Lock()
+			newTodos := createRandomTodoRepository(t)
+			channel <- newTodos
+			mutex.Unlock()
+		}()
+		todos = append(todos, <-channel)
+	}
+
+	todoRepository := repository.NewRepositoryTodo(ConnTest)
+
+	// Find by actiivity group
+	todos, err := todoRepository.FindByActivityGroupID(todos[0].ActivityGroupID)
+	helper.ErrLogPanic(err)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(todos))
+
+	for _, data := range todos {
+		// Equal
+		require.Equal(t, todos[0].ID, data.ID)
+		require.Equal(t, todos[0].Title, data.Title)
+		require.Equal(t, todos[0].ActivityGroupID, data.ActivityGroupID)
+		require.Equal(t, todos[0].IsActive, data.IsActive)
+		require.Equal(t, todos[0].Priority, data.Priority)
+
+		require.Empty(t, data.DeletedAt)
+	}
+
+}
+
+func TestFindOneTodoRepository(t *testing.T) {
+	t.Parallel()
+	newTodo := createRandomTodoRepository(t)
+
+	todoRepository := repository.NewRepositoryTodo(ConnTest)
+
+	// Find One
+	todo, err := todoRepository.FindOne(newTodo.ID)
+	helper.ErrLogPanic(err)
+
+	assert.NoError(t, err)
+
+	// Equal
+	require.Equal(t, todo.ID, newTodo.ID)
+	require.Equal(t, todo.Title, newTodo.Title)
+	require.Equal(t, todo.ActivityGroupID, newTodo.ActivityGroupID)
+	require.Equal(t, todo.IsActive, newTodo.IsActive)
+	require.Equal(t, todo.Priority, newTodo.Priority)
+
+	require.NotEmpty(t, newTodo.CreatedAt)
+	require.NotEmpty(t, newTodo.UpdatedAt)
+	require.Empty(t, newTodo.DeletedAt)
+
+}
+
 func TestCreateTodoRepository(t *testing.T) {
 	t.Parallel()
 	createRandomTodoRepository(t)
+}
+
+func TestUpdateTodoRepository(t *testing.T) {
+	t.Parallel()
+	var mutex sync.Mutex
+	// todos for store `new data todos` from channel
+	var todos []domain.Todo
+
+	// channel for store data `new data todos` from process create random todo
+	channel := make(chan domain.Todo)
+	defer close(channel)
+
+	// Create some random data
+	for i := 0; i < 2; i++ {
+		go func() {
+			mutex.Lock()
+			newTodos := createRandomTodoRepository(t)
+			channel <- newTodos
+			mutex.Unlock()
+		}()
+		todos = append(todos, <-channel)
+	}
+
+	todoRepository := repository.NewRepositoryTodo(ConnTest)
+
+	dataUpdate := domain.Todo{
+		ID:              todos[0].ID,
+		ActivityGroupID: todos[1].ActivityGroupID,
+		Title:           jabufaker.RandomString(20),
+		IsActive:        false,
+		Priority:        helper.RandomPriority(),
+		CreatedAt:       todos[0].CreatedAt,
+		UpdatedAt:       time.Now(),
+		DeletedAt:       nil,
+	}
+
+	// Update
+	todo, err := todoRepository.Update(dataUpdate)
+	helper.ErrLogPanic(err)
+
+	assert.NoError(t, err)
+
+	// Test
+	require.Equal(t, todo.ID, todos[0].ID)
+
+	require.NotEqual(t, todo.Title, todos[0].Title)
+	require.NotEqual(t, todo.ActivityGroupID, todos[0].ActivityGroupID)
+	require.NotEqual(t, todo.IsActive, todos[0].IsActive)
+	require.NotEqual(t, todo.Priority, todos[0].Priority)
+
+	require.NotEmpty(t, todo.CreatedAt)
+	require.NotEmpty(t, todo.UpdatedAt)
+
+	require.Empty(t, todos[0].DeletedAt)
+}
+
+func TestDeleteTodoRepository(t *testing.T) {
+	t.Parallel()
+	newTodo := createRandomTodoRepository(t)
+
+	todoRepository := repository.NewRepositoryTodo(ConnTest)
+
+	// Update
+	ok, err := todoRepository.Delete(newTodo)
+	helper.ErrLogPanic(err)
+
+	assert.NoError(t, err)
+	assert.True(t, ok)
+
+	todo, err := todoRepository.FindOne(newTodo.ID)
+	helper.ErrLogPanic(err)
+	nullId := uint64(0)
+	assert.Equal(t, nullId, todo.ID)
 }
