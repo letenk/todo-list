11499baa2b90021cafa6fd268a44bf0ae14577a0
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-11-02 09:47:11 +0700

    features: create object Todo

--- config/database.go
+++ config/database.go
@@ -50,7 +50,7 @@ func SetupDB() *gorm.DB {
 			counts++
 		} else {
 			// Auto Migrate
-			err = conn.AutoMigrate(&domain.ActivityGroup{})
+			err = conn.AutoMigrate(&domain.ActivityGroup{}, &domain.Todo{})
 
 			if err != nil {
 				log.Fatalf("Failed to auto migration %v", err)
--- models/domain/todo.go
+++ models/domain/todo.go
@@ -1,15 +1,15 @@
 package domain
 
-type Todo struct {
-}
+import "time"
 
-/*
-id": 1,
-            "activity_group_id": "2",
-            "title": "item1",
-            "is_active": "1",
-            "priority": "very-high",
-            "created_at": "2021-11-30T18:09:19.000Z",
-            "updated_at": "2021-11-30T20:23:12.000Z",
-            "deleted_at": null
-*/
+type Todo struct {
+	ID              uint64        `gorm:"primary_key"`
+	ActivityGroupID uint64        `gorm:"not null"`
+	Title           string        `gorm:"type:varchar(191);not null"`
+	IsActive        uint16        `gorm:"not null"`
+	Priority        string        `gorm:"type:varchar(191);not null"`
+	CreatedAt       *time.Time    `gorm:"autoCreateTime"`
+	UpdatedAt       time.Time     `gorm:"autoCreateTime"`
+	DeletedAt       *time.Time    `gorm:"default:null"`
+	ActivityGroup   ActivityGroup // Relation one to many to activity groups
+}
